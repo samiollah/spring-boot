b1cad8b64d04a658441b9378937ea33768cd43bb
sketabifard <smi.ketai@gmail.com>
2018-07-02 17:32:07 +0430

    logging and jersey

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,21 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @EnableAutoConfiguration
+@Configuration
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
     }
     @RequestMapping("/")
      String test() {
-        return "test is ok";
+        return "rest controller with spring boot !";
     }
 }
